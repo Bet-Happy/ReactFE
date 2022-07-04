d063fd4b5ecc2eb8d5511ddd4859ab846d3a6fdc
winstoncwang <congwang@live.co.uk>
2022-07-04 21:35:24 +0100

    initial design

---
+++ src/components/VerticalNavs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function VerticalNavs() {
+    return (
+        <ul className="nav nav-pills flex-column m-auto p-auto text-truncate">
+            <li className="nav-item">
+                <a className="nav-link active text-truncate" aria-current="page" href="#">Dashboard</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white text-truncate" href="#">Mining</a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link text-white text-truncate" href="#">Woodcutting</a>
+            </li>
+        </ul>
+    );
+}
+
+export default VerticalNavs;
