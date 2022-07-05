6838b48b086fffe0b6867433cd3eb105da2c87af
winstoncwang <congwang@live.co.uk>
2022-07-05 17:28:33 +0100

    circle progress bar

--- src/components/ProgressCircle.js
+++ src/components/ProgressCircle.js
@@ -1,20 +1,47 @@
 import React from 'react';
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
+class ProgressCircle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { value: 0 };
+    }
 
-function ProgressCircle(props) {
-    return (
-        <div className='container align-self-center w-50'>
-            <div className="card">
-                <div className="card-header">Activity: {props.skillName}</div>
-                <div className="card-body">
-                    <div className="card-title">progress circle</div>
-                    <div className="card-text">
-                        Description.
+    componentDidMount() {
+        let sec = 10;
+        setInterval(
+            () => {
+                if (this.state.value > 100) {
+                    //console.log(this.state.value)
+                    this.setState({ value: 0 });
+                } else {
+                    this.setState({ value: this.state.value++ })
+                }
+            },
+            100
+        );
+
+    }
+
+    render() {
+        return (
+            <div className='container align-self-center w-50'>
+                <div className="card">
+                    <div className="card-header">Activity: {this.props.skillName}</div>
+                    <div className="card-body">
+                        <div style={{ width: 100, height: 100 }}>
+                            <CircularProgressbar value={this.state.value} maxValue={100} text={`${this.state.value}%`} styles={buildStyles({ pathTransition: this.state.value === 0 ? "none" : "stroke-dashoffset 0.5s ease 0s" })} />
+                        </div>
+                        <div className="card-title">progress circle</div>
+                        <div className="card-text">
+                            Description.
+                        </div>
                     </div>
-                </div>
-            </div >
-        </div>
-    );
+                </div >
+            </div>
+        );
+    }
 
 }
 
