022a7f7ae22ff89d171fed4e4dd6cca6aed3d754
Student <Student@MA403.local>
2022-07-08 16:55:57 +0100

    jazzed up the HomePage

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,11 +1,32 @@
 import React from "react";
+import '../App.css';
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>
-      <p>Level 19</p>
-      <p>Warrior</p>
+      <h2>Character Name Placeholder</h2>
+     
+      <div className='container align-self-center w-75' >
+        <div className="card">
+          <div className='card-header'>Skills</div>
+          <div className='card-body row'>
+            <div id='HomePageSkills'>
+              <div>
+              Mining (0/99)
+              </div>
+              <div>
+              Smithing (0/99)
+              </div>
+              
+              
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
