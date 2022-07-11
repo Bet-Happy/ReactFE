3f937ff429d788460c54439e2f568029de17e54b
Student <Student@MA403.local>
2022-07-11 17:24:32 +0100

    trying to fix async

--- src/App.js
+++ src/App.js
@@ -9,40 +9,63 @@ import Home from './views/HomePage';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 0, active: false, sec: 5, miningXp: 5, activeOre: "none" };
+    this.state = { id: null, value: 0, active: false, sec: 5, miningXp: 5, activeOre: "none" };
   }
 
   async componentDidMount() {
-    const response = await fetch('http://localhost:8080/character', {
+    const response = await fetch('http://localhost:8080/characters', {
       method: 'GET',
       headers: {
         "Content-Type": "application/json"
       }
     });
     const data = await response.json();
-    this.setState({ xp: data['data']['mining'] })
-    console.log(data['data']['mining'])
-    console.log(data)
+    this.setState({ id: data['data'][0]['id']});
+    this.setState({ miningXp: data['data'][0]['mining'] });
+    
   }
 
-  updateXp = () => {
-    this.setState({ miningXp: this.state.miningXp + 1 }) // 1 is a placeholder for the xp from resource table DB
+  async postXp() {
+    const id = this.state['id']
+    const miningContent = this.state['miningXp']
+    let data = {
+      id: id,
+      mining: miningContent
+    };
+    await fetch('http://localhost:8080/characters/updateExperience', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+  }
+
+
+  updateXp = async (e) => {
+    
+   this.setState({ miningXp: this.state.miningXp + 1 }) // 1 is a placeholder for the xp from resource table DB
+
   }
 
   updateActiveMiningSkill = (oreName) => {
     this.setState({ activeOre: oreName });
   }
 
-  startProgress = () => {
+  startProgress = async (e) => {
     console.log("startProgress");
     this.counterInterval = setInterval(
-      () => {
+      async (e) => {
         if (this.state.value >= 100) {
-
           //right here is were we update the Xp state in XpBox.js
-          this.updateXp();
-
           this.setState({ value: 0 });
+          console.log(this.state.miningXp)
+          await this.updateXp();
+          // below should only run after updateXp has run, but currently it runs immediatly
+          console.log(this.state.miningXp);
+          await this.postXp();
+          
+          
         } else {
           this.setState({ value: this.state.value + 1 })
         }
@@ -55,6 +78,7 @@ class App extends React.Component {
     console.log("endProgress");
     clearInterval(this.counterInterval);
     this.setState({ value: 0 })
+    console.log(this.state.miningXp)
   }
 
   render() {
@@ -68,7 +92,7 @@ class App extends React.Component {
             <div className="col-lg-10 col-md-9 col-sm-8">
               <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/Mine" element={<Mine activeOre={this.state.activeOre} value={this.state.value} xp={this.state.xp} startProgress={this.startProgress} endProgress={this.endProgress} activeMiningSkill={this.updateActiveMiningSkill} />} />
+                <Route path="/Mine" element={<Mine activeOre={this.state.activeOre} value={this.state.value} xp={this.state.miningXp} startProgress={this.startProgress} endProgress={this.endProgress} activeMiningSkill={this.updateActiveMiningSkill} />} />
               </Routes>
             </div>
           </div>
