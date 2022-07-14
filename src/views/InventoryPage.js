c61630aefc348000e93c42bc84416222e390cef3
winstoncwang <congwang@live.co.uk>
2022-07-14 13:37:47 +0100

    inventory page refurbished

--- src/views/InventoryPage.js
+++ src/views/InventoryPage.js
@@ -22,33 +22,26 @@ class Inventory extends React.Component {
         const { inventory } = this.state;
         const list = inventory.map((item) => {
             //only display when item.amount > 0
-            if (item.amount > 0) {
+            if (item.amount == 0) { return (<></>); }
+            if (item.resource === "Copper" && item.amount >= 1) {
                 return (
-                    <div className='container-fluid text-white' key={item.id}>
+                    <div className='container-fluid text-white box' key={item.id}>
+                        <img src={require("./icons/copperOre.png")} className="img-thumbnail" alt="copper img" />
+                        <br></br>
                         {item.resource}
                         <br></br>
-                        {item.amount}
-                    </div>
-                )
-            }
-
-            if (item.resource === "Copper") {
-                return (
-                    <div className='container-fluid text-white' key={item.id}>
-                        <img src={require("./icons/copperOre.png")} className="img-thumbnail round" alt="copper img" />
-                        {item.resource}
-                        <br></br>
-                        <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                        {/* <span class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
                             <span class="visually-hidden">New alerts</span>
-                        </span>
+                        </span> */}
                         {item.amount}
                     </div >
                 )
             }
-            if (item.resource === "Tin") {
+            if (item.resource === "Tin" && item.amount >= 1) {
                 return (
-                    <div className='container-fluid text-white' key={item.id}>
-                        <img src={require("./icons/tinOre.png")} className="img-thumbnail round" alt="tin img" />
+                    <div className='container-fluid text-white box' key={item.id}>
+                        <img src={require("./icons/tinOre.png")} className="img-thumbnail" alt="tin img" />
+                        <br></br>
                         {item.resource}
                         <br></br>
                         {item.amount}
@@ -57,14 +50,10 @@ class Inventory extends React.Component {
             }
 
         });
-
-
         return (
-            <div className='container-fluid p-0 m-0'>
-                <div className='inventory-container'>
-
+            <div className='container-fluid inventory p-0 p-5'>
+                <div className='container inventory-grid m-auto p-4'>
                     {list}
-
                 </div>
             </div>
         );
